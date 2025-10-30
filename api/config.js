2b66179ec78c7f22f56bccb4f9a88a1d7022db4b
Ricardo ANNIS <123905385+Ricdrum14@users.noreply.github.com>
2025-10-30 15:53:33 +0100

    Ajout des modifs de configurations

--- api/config.js
+++ api/config.js
@@ -1,11 +1,11 @@
 module.exports =  {
     ACCESS_TOKEN_SECRET : "EMMA123",
     BDD : {
-    "host" :"dpg-d40798bipnbc73cf4njg-a.oregon-postgres.render.com",
+    "host" :"dpg-d41mvg24d50c73eto4vg-a",
     "port" : "5432",
-    "user" : "cnam_qnjc_user",
-    "password" : "FOYjmLxOmqwdnfGPhMO4R8eZpErPzVXQ",
-    "bdname" :"cnam_qnjc" 
+    "user" : "pollution_bpbo_user",
+    "password" : "2BZl1iWB49PZmtA2ZkYkZCmtI9U4Jcur",
+    "bdname" :"pollution_bpbo" 
     }
 }
 
