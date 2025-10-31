3bbeb77cf59a210b7936cacc173cc66f018bc310
Ricardo ANNIS <123905385+Ricdrum14@users.noreply.github.com>
2025-10-31 15:29:37 +0100

    Ajout derniers modifs

--- api/controllers/pollution.controllers.js
+++ api/controllers/pollution.controllers.js
@@ -1,34 +1,34 @@
-const { v4: uuidv4 } = require ("uuid");
+// const { v4: uuidv4 } = require ("uuid");
 
 
 const db = require("../models");
 const Pollution = db.pollution;
 const Op = db.Sequelize.Op;
 
-// exports.get = (req, res) => {
+exports.get = (req, res) => {
 
-//      Pollution.get()
-//     .then(data => {res.send(data);})
-//     .catch(err => {
-//       res.status(400).send({
-//         message: err.message
-//       });
-//     });
+     Pollution.get()
+    .then(data => {res.send(data);})
+    .catch(err => {
+      res.status(400).send({
+        message: err.message
+      });
+    });
 
-// }; 
+}; 
 
 
-exports.get= (req, res) => {
-    const type = req.query.typePollution
-    const condition = type ? { typePollution: type } : null
+// exports.get= (req, res) => {
+//     const type = req.query.typePollution
+//     const condition = type ? { typePollution: type } : null
 
-    Pollution.findAll({ where: condition })
-        .then(data => {
-            res.send(data)
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: err.message || 'Some error occurred while retrieving pollutions.'
-            })
-        })
-}
+//     Pollution.findAll({ where: condition })
+//         .then(data => {
+//             res.send(data)
+//         })
+//         .catch(err => {
+//             res.status(500).send({
+//                 message: err.message || 'Some error occurred while retrieving pollutions.'
+//             })
+//         })
+// }
