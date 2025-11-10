f43243d7375ea870368c0028e3191b8aa33bb0df
Ricardo ANNIS <123905385+Ricdrum14@users.noreply.github.com>
2025-11-10 18:21:21 +0100

    Mise a jour de mon API 18:21

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -1,6 +1,8 @@
 import { Application } from 'express';
 import pollutionRoutes from './pollution.routes';
+import utilisateurRoutes from './utilisateur.routes';
 
 export default (app: Application): void => {
     pollutionRoutes(app);
+    utilisateurRoutes(app);
 };
