a8a6edacc72181e71dca2e5d0bde9ead8b6ae431
Ricardo ANNIS <123905385+Ricdrum14@users.noreply.github.com>
2025-11-10 18:34:11 +0100

    Mise a jour de mon API 18:34

--- api/models/utilisateur.model.ts
+++ api/models/utilisateur.model.ts
@@ -24,43 +24,51 @@ export interface UtilisateurInstance
 
 // Fonction d'initialisation du modèle
 export default function (sequelize: Sequelize): ModelStatic<UtilisateurInstance> {
-  const Utilisateur = sequelize.define<UtilisateurInstance>('utilisateur', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    nom: {
-      type: DataTypes.STRING(100),
-      allowNull: false
-    },
-    prenom: {
-      type: DataTypes.STRING(100),
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING(150),
-      allowNull: false,
-      unique: true,
-      validate: { isEmail: true }
-    },
-    mot_de_passe: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    },
-    role: {
-      type: DataTypes.STRING(20),
-      allowNull: false,
-      defaultValue: 'utilisateur',
-      validate: {
-        isIn: [['admin', 'utilisateur']]
+  const Utilisateur = sequelize.define<UtilisateurInstance>(
+    'utilisateur',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      nom: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+      },
+      prenom: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING(150),
+        allowNull: false,
+        unique: true,
+        validate: { isEmail: true }
+      },
+      mot_de_passe: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+      },
+      role: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+        defaultValue: 'utilisateur',
+        validate: {
+          isIn: [['admin', 'utilisateur']]
+        }
+      },
+      date_creation: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
       }
     },
-    date_creation: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW
+    {
+      tableName: 'utilisateur', 
+      timestamps: false          
     }
-  });
+  );
+
 
   //Hooks de hashage du mot de passe
   Utilisateur.beforeCreate(async (user) => {
