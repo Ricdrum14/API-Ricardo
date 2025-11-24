e16a670cb9182c2b17509913000270a8863b3353
Ricardo ANNIS <123905385+Ricdrum14@users.noreply.github.com>
2025-11-24 13:46:01 +0100

    Modif de l'api

--- api/config.ts
+++ api/config.ts
@@ -14,12 +14,13 @@ interface Config {
 const config: Config = {
   ACCESS_TOKEN_SECRET: "EMMA123",
   BDD: {
-    host: "dpg-d41mvg24d50c73eto4vg-a.singapore-postgres.render.com",
+    host: "dpg-d4i4pr95pdvs739jg100-a.oregon-postgres.render.com",
     port: "5432",
-    user: "pollution_bpbo_user",
-    password: "2BZl1iWB49PZmtA2ZkYkZCmtI9U4Jcur",
-    bdname: "pollution_bpbo"
+    user: "pollution_wsrw_user",
+    password: "JQ8xRJIycJi09rX6ni8WW3nEt7JoGt7b",
+    bdname: "pollution_wsrw"
   }
+
 };
 
 export default config;
